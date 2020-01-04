5e44af030750b758561a7f21d2c419a42b3c3ebb
Ahmed Abou Aliaa <ahmed.s.aboualiaa@gmail.com>
2020-01-04 01:30:20 +0100

    add script file to install hpx

---
+++ ci-scripts/install-hpx.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ -d ./packages/hpx/cmake-build-release ]; then
+    echo "cached hpx found -- nothing to download, installing";
+    cd ./packages/hpx/cmake-build-release;
+    timeout 15m make -j$(nproc) install;
+    cd ../../../;
+    else
+    git clone https://github.com/STEllAR-GROUP/hpx.git packages/hpx;
+    cd ./packages/hpx;
+    mkdir cmake-build-release && cd ./cmake-build-release;
+    cmake .. -DJEMALLOC_ROOT=/usr/local/opt/jemalloc -DHPX_WITH_MALLOC=jemalloc;
+    timeout 15m make -j$(nproc) install;
+    cd ../../../;
+    fi
