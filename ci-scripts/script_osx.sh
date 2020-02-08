ce97f979687a1df0b39dffc88578d871cf36d2b1
Ahmed Abou-Aliaa <ahmed.s.aboualiaa@gmail.com>
2020-02-09 00:16:54 +0100

    [ci:bug] correct paths for sed, shallow git clones

--- ci-scripts/script_osx.sh
+++ ci-scripts/script_osx.sh
@@ -2,10 +2,10 @@
 
 sudo xcode-select -r # s /Applications/Xcode.app
 mkdir -p cmake-build-release && cd ./cmake-build-release
-sed -i '' '/CMAKE_CXX_COMPILER:FILEPATH/d' ./cmake-build-release/CMakeCache.txt # remove compiler cache entry just in case travis updated xcode
-sed -i '' '/\/\/CXX compiler/d' ./cmake-build-release/CMakeCache.txt
-sed -i '' '/CMAKE_C_COMPILER:FILEPATH/d' ./cmake-build-release/CMakeCache.txt # remove compiler cache entry just in case travis updated xcode
-sed -i '' '/\/\/C compiler/d' ./cmake-build-release/CMakeCache.txt
+sed -i '' '/CMAKE_CXX_COMPILER:FILEPATH/d' ./CMakeCache.txt # remove compiler cache entry just in case travis updated xcode
+sed -i '' '/\/\/CXX compiler/d' ./CMakeCache.txt
+sed -i '' '/CMAKE_C_COMPILER:FILEPATH/d' ./CMakeCache.txt # remove compiler cache entry just in case travis updated xcode
+sed -i '' '/\/\/C compiler/d' ./CMakeCache.txt
 cmake -G Ninja -DGEMS_BUILD_MATLAB=OFF ..
 cmake -G Ninja -DGEMS_BUILD_MATLAB=OFF .. # again so that AppleClang finds OpenMP
 timeout 30m ninja
