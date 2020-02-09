d050f78c9de354a5e62b6750bc622d1166201a4b
Ahmed Abou-Aliaa <ahmed.s.aboualiaa@gmail.com>
2020-02-09 23:38:15 +0100

    [ci:bug] pass -p to mkdir to prevent errors

--- ci-scripts/configure.trusty.sh
+++ ci-scripts/configure.trusty.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-mkdir cmake-build-debug-trusty && cd ./cmake-build-debug-trusty
+(
+mkdir -p cmake-build-debug-trusty && cd ./cmake-build-debug-trusty
 
 sudo apt-get install -y --no-install-suggests --no-install-recommends git wget tcsh build-essential \
   gfortran libblas-dev liblapack-dev zlib1g-dev libxmu-dev \
   libxmu-headers libxi-dev libxt-dev libx11-dev libglu1-mesa-dev \
   mpich2 python3.5 libboost-all-dev libfltk1.3-dev libeigen3-dev \
   libarmadillo-dev qt5-default libqt5x11extras5-dev
-
-#sudo mv /usr/bin/python /usr/bin/python.bak
-#sudo mv /usr/bin/python3.5 /usr/bin/python
 
 #sudo update-alternatives --install /usr/local/bin/python python /usr/bin/python2.7 2
 sudo update-alternatives --install /usr/local/bin/python python /usr/bin/python3.5 1 --force
@@ -23,7 +21,7 @@ sudo apt-get install -y gcc-9 g++-9
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-9 90 --slave /usr/bin/g++ g++ /usr/bin/g++-9
 
 wget https://cmake.org/files/v3.16/cmake-3.16.4-Linux-x86_64.tar.gz
-tar -xzvf cmake-3.16.4-Linux-x86_64.tar.gz
+tar -xzf cmake-3.16.4-Linux-x86_64.tar.gz
 export PATH="$(pwd)/cmake-3.16.4-Linux-x86_64/bin:$PATH"
 
 wget https://github.com/ninja-build/ninja/releases/download/v1.8.2/ninja-linux.zip
@@ -45,3 +43,4 @@ cmake -DCMAKE_CROSSCOMPILING=1 -DRUN_HAVE_STD_REGEX=0 -DRUN_HAVE_POSIX_REGEX=0 .
 #./configure
 #make all test
 #make install
+)
