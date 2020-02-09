d6d0f5a69b57508df4e295b00b5a7f0e6834b830
Ahmed Abou-Aliaa <ahmed.s.aboualiaa@gmail.com>
2020-02-09 12:31:52 +0100

    [ci:ref] increase hpx timeout to 50 min

--- ci-scripts/install-hpx.sh
+++ ci-scripts/install-hpx.sh
@@ -3,13 +3,13 @@
 if [ -d ./packages/hpx/cmake-build-release ]; then
     echo "cached hpx found -- nothing to download, installing";
     cd ./packages/hpx/cmake-build-release;
-    timeout 30m make -j$(nproc) install;
+    timeout 50m make -j$(nproc) install;
     cd ../../../;
     else
     git clone https://github.com/STEllAR-GROUP/hpx.git packages/hpx;
     cd ./packages/hpx;
     mkdir cmake-build-release && cd ./cmake-build-release;
     cmake .. -DJEMALLOC_ROOT=/usr/local/opt/jemalloc -DHPX_WITH_MALLOC=jemalloc;
-    timeout 30m make -j$(nproc) install;
+    timeout 50m make -j$(nproc) install;
     cd ../../../;
     fi
