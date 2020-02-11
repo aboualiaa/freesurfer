d463d748ba5c421ceba173ec3e389ca9cdd6ed49
Ahmed Abou-Aliaa <ahmed.s.aboualiaa@gmail.com>
2020-02-11 11:58:10 +0100

    [ci:bug] fixing compilers for linux jobs

--- ci-scripts/travis.configure.xenial.clang.sh
+++ ci-scripts/travis.configure.xenial.clang.sh
@@ -8,12 +8,77 @@
     libxmu-headers libxi-dev libxt-dev libx11-dev libglu1-mesa-dev \
     mpich python3.5 libboost-all-dev libfltk1.3-dev libeigen3-dev \
     libarmadillo-dev qt5-default libqt5x11extras5-dev curl
+  #
+  #  curl -SL http://releases.llvm.org/9.0.0/clang%2bllvm-9.0.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz | tar -xJC .
+  #  mv clang+llvm-9.0.0-x86_64-linux-gnu-ubuntu-16.04 clang_9.0.0
+  #  sudo mv clang_9.0.0 /usr/local
+  #  export PATH="/usr/local/clang_9.0.0/bin:$PATH"
+  #  export LD_LIBRARY_PATH="/usr/local/clang_9.0.0/lib:$LD_LIBRARY_PATH"
 
-  curl -SL http://releases.llvm.org/9.0.0/clang%2bllvm-9.0.0-x86_64-linux-gnu-ubuntu-16.04.tar.xz | tar -xJC .
-  mv clang+llvm-9.0.0-x86_64-linux-gnu-ubuntu-16.04 clang_9.0.0
-  sudo mv clang_9.0.0 /usr/local
-  export PATH="/usr/local/clang_9.0.0/bin:$PATH"
-  export LD_LIBRARY_PATH="/usr/local/clang_9.0.0/lib:$LD_LIBRARY_PATH"
+  # To Install llvm/clang 9.0
+  wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
+  sudo apt-add-repository -y "deb http://apt.llvm.org/xenial/ llvm-toolchain-xenial-9.0 main"
+  sudo apt update
+  sudo apt install -y clang-9.0 llvm-9.0 lld-9.0 lldb-9.0
+  sudo apt install clang-{format,tidy,tools}-9.0 clang-9.0-doc clang-9.0-examples
+
+  # Remove all existing alternatives
+  sudo update-alternatives --remove-all llvm
+  sudo update-alternatives --remove-all clang
+  sudo update-alternatives --remove-all gcc
+
+  # llvm-9.0 and clang-9.0
+  sudo update-alternatives \
+    --install /usr/lib/llvm llvm /usr/lib/llvm-9.0 20 \
+    --slave /usr/bin/llvm-config llvm-config /usr/bin/llvm-config-9.0 \
+    --slave /usr/bin/llvm-ar llvm-ar /usr/bin/llvm-ar-9.0 \
+    --slave /usr/bin/llvm-as llvm-as /usr/bin/llvm-as-9.0 \
+    --slave /usr/bin/llvm-bcanalyzer llvm-bcanalyzer /usr/bin/llvm-bcanalyzer-9.0 \
+    --slave /usr/bin/llvm-cov llvm-cov /usr/bin/llvm-cov-9.0 \
+    --slave /usr/bin/llvm-diff llvm-diff /usr/bin/llvm-diff-9.0 \
+    --slave /usr/bin/llvm-dis llvm-dis /usr/bin/llvm-dis-9.0 \
+    --slave /usr/bin/llvm-dwarfdump llvm-dwarfdump /usr/bin/llvm-dwarfdump-9.0 \
+    --slave /usr/bin/llvm-extract llvm-extract /usr/bin/llvm-extract-9.0 \
+    --slave /usr/bin/llvm-link llvm-link /usr/bin/llvm-link-9.0 \
+    --slave /usr/bin/llvm-mc llvm-mc /usr/bin/llvm-mc-9.0 \
+    --slave /usr/bin/llvm-mcmarkup llvm-mcmarkup /usr/bin/llvm-mcmarkup-9.0 \
+    --slave /usr/bin/llvm-nm llvm-nm /usr/bin/llvm-nm-9.0 \
+    --slave /usr/bin/llvm-objdump llvm-objdump /usr/bin/llvm-objdump-9.0 \
+    --slave /usr/bin/llvm-ranlib llvm-ranlib /usr/bin/llvm-ranlib-9.0 \
+    --slave /usr/bin/llvm-readobj llvm-readobj /usr/bin/llvm-readobj-9.0 \
+    --slave /usr/bin/llvm-rtdyld llvm-rtdyld /usr/bin/llvm-rtdyld-9.0 \
+    --slave /usr/bin/llvm-size llvm-size /usr/bin/llvm-size-9.0 \
+    --slave /usr/bin/llvm-stress llvm-stress /usr/bin/llvm-stress-9.0 \
+    --slave /usr/bin/llvm-symbolizer llvm-symbolizer /usr/bin/llvm-symbolizer-9.0 \
+    --slave /usr/bin/llvm-tblgen llvm-tblgen /usr/bin/llvm-tblgen-9.0
+
+  sudo update-alternatives \
+    --install /usr/bin/clang clang /usr/bin/clang-9.0 20 \
+    --slave /usr/bin/clang++ clang++ /usr/bin/clang++-9.0 \
+    --slave /usr/bin/lld lld /usr/bin/lld-9.0 \
+    --slave /usr/bin/clang-format clang-format /usr/bin/clang-format-9.0 \
+    --slave /usr/bin/clang-tidy clang-tidy /usr/bin/clang-tidy-9.0 \
+    --slave /usr/bin/clang-tidy-diff.py clang-tidy-diff.py /usr/bin/clang-tidy-diff-9.0.py \
+    --slave /usr/bin/clang-include-fixer clang-include-fixer /usr/bin/clang-include-fixer-9.0 \
+    --slave /usr/bin/clang-offload-bundler clang-offload-bundler /usr/bin/clang-offload-bundler-9.0 \
+    --slave /usr/bin/clangd clangd /usr/bin/clangd-9.0 \
+    --slave /usr/bin/clang-check clang-check /usr/bin/clang-check-9.0 \
+    --slave /usr/bin/scan-view scan-view /usr/bin/scan-view-9.0 \
+    --slave /usr/bin/clang-apply-replacements clang-apply-replacements /usr/bin/clang-apply-replacements-9.0 \
+    --slave /usr/bin/clang-query clang-query /usr/bin/clang-query-9.0 \
+    --slave /usr/bin/modularize modularize /usr/bin/modularize-9.0 \
+    --slave /usr/bin/sancov sancov /usr/bin/sancov-9.0 \
+    --slave /usr/bin/c-index-test c-index-test /usr/bin/c-index-test-9.0 \
+    --slave /usr/bin/clang-reorder-fields clang-reorder-fields /usr/bin/clang-reorder-fields-9.0 \
+    --slave /usr/bin/clang-change-namespace clang-change-namespace /usr/bin/clang-change-namespace-9.0 \
+    --slave /usr/bin/clang-import-test clang-import-test /usr/bin/clang-import-test-9.0 \
+    --slave /usr/bin/scan-build scan-build /usr/bin/scan-build-9.0 \
+    --slave /usr/bin/scan-build-py scan-build-py /usr/bin/scan-build-py-9.0 \
+    --slave /usr/bin/clang-cl clang-cl /usr/bin/clang-cl-9.0 \
+    --slave /usr/bin/clang-rename clang-rename /usr/bin/clang-rename-9.0 \
+    --slave /usr/bin/find-all-symbols find-all-symbols /usr/bin/find-all-symbols-9.0 \
+    --slave /usr/bin/lldb lldb /usr/bin/lldb-9.0 \
+    --slave /usr/bin/lldb-server lldb-server /usr/bin/lldb-server-9.0
 
   export PATH="/usr/local/bin:$PATH"
 
