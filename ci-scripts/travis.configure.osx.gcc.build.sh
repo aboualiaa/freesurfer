d93a5d824860d219bc14fd7b904793585aca129c
Ahmed Abou-Aliaa <ahmed.s.aboualiaa@gmail.com>
2020-02-12 06:20:24 +0100

    [ci:bug] ignore itk error on gcc 9

--- ci-scripts/travis.configure.osx.gcc.build.sh
+++ ci-scripts/travis.configure.osx.gcc.build.sh
@@ -2,5 +2,6 @@
 
 ln -s /usr/local/opt/gcc/gcc-9 /usr/local/opt/gcc/bin/gcc
 ln -s /usr/local/opt/gcc/g++-9 /usr/local/opt/gcc/bin/g++
+sudo sed -i -- 's/#  error \"Dunno about this gcc\"/\/\//g' /usr/local/include/ITK-5.0/itk_compiler_detection.h
 ./ci-scripts/restore-mtime.sh
 ./ci-scripts/install-hpx.sh
