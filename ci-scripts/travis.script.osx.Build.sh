abd3a1959cab8b6f445b654eeddac4fd14c3c413
Ahmed Abou-Aliaa <ahmed.s.aboualiaa@gmail.com>
2020-02-12 10:24:56 +0100

    [ci:bug] reducing travis cache size, fix issues with git annex

--- ci-scripts/travis.script.osx.Build.sh
+++ ci-scripts/travis.script.osx.Build.sh
@@ -11,10 +11,12 @@ cmake -G Ninja -DCMAKE_BUILD_TYPE=Debug -DGEMS_BUILD_MATLAB=OFF .. # again so th
 timeout 50m ninja -k 0
 cd ..
 git remote add datasrc https://surfer.nmr.mgh.harvard.edu/pub/dist/freesurfer/repo/annex.git
+git annex init
+git fetch datasrc
 git annex get . || true
 cd ./cmake-build-debug
 cmake ..
-ctest --timeout 300 -j 12
+ctest --timeout 300 -j $(nproc)
 lcov --capture --directory . --output-file ./coverage.info
 bash <(curl -s https://codecov.io/bash) -f ./coverage.info -t ${CODECOV_TOKEN}
 rm -f ./**/testdata.tar.gz
