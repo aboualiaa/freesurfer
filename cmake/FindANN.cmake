f7f4380ec4407c191f46f1411977bf97ffe89d1c
Ahmed Abou-Aliaa <ahmed.s.aboualiaa@gmail.com>
2020-02-12 16:02:52 +0100

    [ci:bug] travis stores the cache before the 'after_success' script smh

--- cmake/FindANN.cmake
+++ cmake/FindANN.cmake
@@ -5,5 +5,5 @@ if(NOT ANN_DIR)
 endif()
 
 find_path(ANN_INCLUDE_DIR HINTS ${ANN_DIR} NAMES ANN PATH_SUFFIXES include)
-find_library(ANN_LIBRARIES HINTS ${ANN_DIR} NAMES libANN.a PATH_SUFFIXES lib)
+find_library(ANN_LIBRARIES HINTS ${ANN_DIR} NAMES libANN.dylib PATH_SUFFIXES lib)
 find_package_handle_standard_args(ANN DEFAULT_MSG ANN_INCLUDE_DIR ANN_LIBRARIES)
