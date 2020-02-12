6f8a7b9fe072300da73173bcbb1034370dadc86b
Ahmed Abou-Aliaa <ahmed.s.aboualiaa@gmail.com>
2020-02-12 07:03:59 +0100

    [ci:bug] build hpx in debug mode

--- ci-scripts/install-hpx.sh
+++ ci-scripts/install-hpx.sh
@@ -10,7 +10,7 @@ else
   git clone https://github.com/STEllAR-GROUP/hpx.git packages/hpx
   cd ./packages/hpx
   mkdir cmake-build-debug && cd ./cmake-build-debug
-  cmake .. -DJEMALLOC_ROOT=/usr/local/opt/jemalloc -DHPX_WITH_MALLOC=jemalloc
+  cmake .. -DCMAKE_BUILD_TYPE=Debug -DJEMALLOC_ROOT=/usr/local/opt/jemalloc -DHPX_WITH_MALLOC=jemalloc
   timeout 50m make -j$(nproc) install
   cd ../../../
 fi
