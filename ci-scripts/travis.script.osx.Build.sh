7544508930599f692865749430b082004f046ede
Ahmed Abou-Aliaa <ahmed.s.aboualiaa@gmail.com>
2020-02-12 14:35:05 +0100

    [ci:bug] fix erroring commands, remove wrong commands

--- ci-scripts/travis.script.osx.Build.sh
+++ ci-scripts/travis.script.osx.Build.sh
@@ -5,10 +5,10 @@ set -e
 sudo xcode-select -r # s /Applications/Xcode.app
 export PATH="$(pwd)/hpx-install/:$PATH"
 mkdir -p cmake-build-debug && cd ./cmake-build-debug
-sed -i '' '/CMAKE_CXX_COMPILER:FILEPATH/d' ./CMakeCache.txt # remove compiler cache entry just in case travis updated xcode
-sed -i '' '/\/\/CXX compiler/d' ./CMakeCache.txt
-sed -i '' '/CMAKE_C_COMPILER:FILEPATH/d' ./CMakeCache.txt # remove compiler cache entry just in case travis updated xcode
-sed -i '' '/\/\/C compiler/d' ./CMakeCache.txt
+sed -i '' '/CMAKE_CXX_COMPILER:FILEPATH/d' ./CMakeCache.txt || true # remove compiler cache entry just in case travis updated xcode
+sed -i '' '/\/\/CXX compiler/d' ./CMakeCache.txt || true
+sed -i '' '/CMAKE_C_COMPILER:FILEPATH/d' ./CMakeCache.txt || true # remove compiler cache entry just in case travis updated xcode
+sed -i '' '/\/\/C compiler/d' ./CMakeCache.txt || true
 cmake -G Ninja -DCMAKE_BUILD_TYPE=Debug -DGEMS_BUILD_MATLAB=OFF ..
 cmake -G Ninja -DCMAKE_BUILD_TYPE=Debug -DGEMS_BUILD_MATLAB=OFF .. # again so that AppleClang finds OpenMP
 timeout 50m ninja -k 0
@@ -18,7 +18,3 @@ git fetch datasrc
 git annex get . || true
 cd ./cmake-build-debug
 cmake ..
-ctest --schedule-random --timeout 150 -j 6
-lcov --capture --directory . --output-file ./coverage.info
-bash <(curl -s https://codecov.io/bash) -f ./coverage.info -t ${CODECOV_TOKEN}
-rm -f ./**/testdata.tar.gz
