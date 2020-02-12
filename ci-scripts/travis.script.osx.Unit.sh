74467b8b80589aaa1c264972ac150c30884b6062
Ahmed Abou-Aliaa <ahmed.s.aboualiaa@gmail.com>
2020-02-12 07:12:18 +0100

    [ci:bug] add missing cmake step in unit stage

--- ci-scripts/travis.script.osx.Unit.sh
+++ ci-scripts/travis.script.osx.Unit.sh
@@ -2,4 +2,5 @@
 
 sudo xcode-select -r # s /Applications/Xcode.app
 mkdir -p cmake-build-debug && cd ./cmake-build-debug
+cmake .. # reconfigure to copy testdata from git-annex
 ctest --timeout 300 -j 12
