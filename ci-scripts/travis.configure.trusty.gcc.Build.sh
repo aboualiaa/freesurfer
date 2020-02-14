d66c6d2202d84a5bae9d4794a24cb6ee01b19d2c
Ahmed Abou-Aliaa <ahmed.s.aboualiaa@gmail.com>
2020-02-14 01:05:58 +0100

    [ci:bug] redirect wget output to /dev/null

--- ci-scripts/travis.configure.trusty.gcc.Build.sh
+++ ci-scripts/travis.configure.trusty.gcc.Build.sh
@@ -22,15 +22,15 @@
   sudo apt-get update
   sudo apt-get install --yes python3.5 python3.5-dev
 
-  wget https://cmake.org/files/v3.16/cmake-3.16.4-Linux-x86_64.tar.gz > /dev/null
+  wget https://cmake.org/files/v3.16/cmake-3.16.4-Linux-x86_64.tar.gz > /dev/null  2>&1
   tar -xzf cmake-3.16.4-Linux-x86_64.tar.gz
   export PATH="$(pwd)/cmake-3.16.4-Linux-x86_64/bin:$PATH"
 
-  wget https://github.com/ninja-build/ninja/releases/download/v1.10.0/ninja-linux.zip > /dev/null
+  wget https://github.com/ninja-build/ninja/releases/download/v1.10.0/ninja-linux.zip > /dev/null  2>&1
   sudo unzip ninja-linux.zip -d /usr/local/bin/
   sudo update-alternatives --install /usr/bin/ninja ninja /usr/local/bin/ninja 1 --force
 
-  wget "https://www.dropbox.com/s/5xffk87vm0wb938/linux_packages.tar?dl=1" > /dev/null
+  wget "https://www.dropbox.com/s/5xffk87vm0wb938/linux_packages.tar?dl=1" > /dev/null  2>&1
   tar -xf "./linux_packages.tar?dl=1"
 
   sed -i -- 's/#  error \"Dunno about this gcc\"/\/\//g' ./prebuilt_packages/itk/include/ITK-4.13/vcl_compiler.h
--- ci-scripts/travis.configure.xenial.gcc.Build.sh
+++ ci-scripts/travis.configure.xenial.gcc.Build.sh
@@ -29,15 +29,15 @@
   tar -xzf cmake-3.16.4-Linux-x86_64.tar.gz
   export PATH="$(pwd)/cmake-3.16.4-Linux-x86_64/bin:$PATH"
 
-  wget https://github.com/ninja-build/ninja/releases/download/v1.10.0/ninja-linux.zip > /dev/null
+  wget https://github.com/ninja-build/ninja/releases/download/v1.10.0/ninja-linux.zip > /dev/null  2>&1
   sudo unzip ninja-linux.zip -d /usr/local/bin/
   sudo update-alternatives --install /usr/bin/ninja ninja /usr/local/bin/ninja 1 --force
 
-  wget "https://www.dropbox.com/s/5xffk87vm0wb938/linux_packages.tar?dl=1" > /dev/null
+  wget "https://www.dropbox.com/s/5xffk87vm0wb938/linux_packages.tar?dl=1" > /dev/null  2>&1
   tar -xf "./linux_packages.tar?dl=1"
 
   rm -rf ./prebuilt_packages/itk
-  wget "https://www.dropbox.com/s/drijzemquap0rqk/itk5.0.1_linux_xenial.tar?dl=1" > /dev/null
+  wget "https://www.dropbox.com/s/drijzemquap0rqk/itk5.0.1_linux_xenial.tar?dl=1" > /dev/null  2>&1
   tar -xf "./itk5.0.1_linux_xenial.tar?dl=1"
   mv ./itk5 ./prebuilt_packages/itk
 
