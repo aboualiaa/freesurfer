7ee469f26164779e30588a08aff42b8fda1b7f32
Ahmed Abou-Aliaa <ahmed.s.aboualiaa@gmail.com>
2020-02-15 14:29:56 +0100

    [ci:bug] fixing scripts of unit test stage, add missing flag for macos

--- ci-scripts/travis.configure.osx.appleclang.Unit.sh
+++ ci-scripts/travis.configure.osx.appleclang.Unit.sh
@@ -4,5 +4,12 @@ sudo xcode-select -r # s /Applications/Xcode.app
 git remote add datasrc https://surfer.nmr.mgh.harvard.edu/pub/dist/freesurfer/repo/annex.git
 git fetch datasrc
 git annex get . || true
-cd ./cmake-build-debug
-cmake ..
+
+export PATH="$(pwd)/hpx-install/:$PATH"
+mkdir -p cmake-build-debug && cd ./cmake-build-debug
+sed -i '' '/CMAKE_CXX_COMPILER:FILEPATH/d' ./CMakeCache.txt || true # remove compiler cache entry just in case travis updated xcode
+sed -i '' '/\/\/CXX compiler/d' ./CMakeCache.txt || true
+sed -i '' '/CMAKE_C_COMPILER:FILEPATH/d' ./CMakeCache.txt || true # remove compiler cache entry just in case travis updated xcode
+sed -i '' '/\/\/C compiler/d' ./CMakeCache.txt || true
+cmake -G Ninja -DCMAKE_BUILD_TYPE=Debug -DGEMS_BUILD_MATLAB=OFF ..
+cmake -G Ninja -DCMAKE_BUILD_TYPE=Debug -DGEMS_BUILD_MATLAB=OFF .. # again so that AppleClang finds OpenMP
--- ci-scripts/travis.script.osx.Unit.sh
+++ ci-scripts/travis.script.osx.Unit.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 cd ./cmake-build-debug;
-ctest --schedule-random --timeout 120 -j 6;
+timeout 50m ninja
+ctest --schedule-random --timeout 90 -j $(nproc) -LE Expensive -T Test;
 lcov --capture --directory . --output-file ./coverage.info;
 bash <(curl -s https://codecov.io/bash) -f ./coverage.info -t ${CODECOV_TOKEN};
 rm -f ./**/testdata.tar.gz;
 rm -rf ./**/testdata/;
+ctest -T Submit;
