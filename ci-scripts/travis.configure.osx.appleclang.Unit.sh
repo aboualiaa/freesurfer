c22a754da12ac22250cfac50cfb7f64805935a2c
Ahmed Abou-Aliaa <ahmed.s.aboualiaa@gmail.com>
2020-02-16 20:46:39 +0100

    [ci:bug] restore mtime and install hpx in osx unit testing stage

--- ci-scripts/travis.configure.osx.appleclang.Unit.sh
+++ ci-scripts/travis.configure.osx.appleclang.Unit.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+./ci-scripts/restore-mtime.sh
+./ci-scripts/install-hpx.sh
+
 sudo xcode-select -r # s /Applications/Xcode.app
 git remote add datasrc https://surfer.nmr.mgh.harvard.edu/pub/dist/freesurfer/repo/annex.git
 git fetch datasrc
