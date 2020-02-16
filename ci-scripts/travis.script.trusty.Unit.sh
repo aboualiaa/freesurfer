62a89e6df6953ef73de15160a576d5fed26a34f9
Ahmed Abou-Aliaa <ahmed.s.aboualiaa@gmail.com>
2020-02-16 19:28:41 +0100

    [ci:bug] disable ccache on osx; update to codecov only from osx

--- ci-scripts/travis.script.trusty.Unit.sh
+++ ci-scripts/travis.script.trusty.Unit.sh
@@ -3,10 +3,10 @@
 cd ./cmake-build-debug-trusty;
 timeout 50m ninja
 ctest --schedule-random --timeout 90 -j $(nproc) -LE Expensive -T Test;
-lcov --capture --directory . --output-file ./coverage.info;
-curl -s https://codecov.io/bash -o codecov.sh
-chmod +x ./codecov.sh
-./codecov.sh -f ./coverage.info -t ${CODECOV_TOKEN};"
+#lcov --capture --directory . --output-file ./coverage.info;
+#curl -s https://codecov.io/bash -o codecov.sh
+#chmod +x ./codecov.sh
+#./codecov.sh -f ./coverage.info -t ${CODECOV_TOKEN};"
 rm -f ./**/testdata.tar.gz;
 rm -rf ./**/testdata/;
 ctest -T Submit;
