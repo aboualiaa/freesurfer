0f0b405796716aa5f503c2a6c40ec3fd68a30128
Ahmed Abou-Aliaa <ahmed.s.aboualiaa@gmail.com>
2020-03-04 19:16:47 +0100

    [ci:bug] fix if conditions

--- ci-scripts/install_deps.sh
+++ ci-scripts/install_deps.sh
@@ -15,7 +15,7 @@ sudo apt-get install -y -qq --no-install-suggests --no-install-recommends git wg
   mpich libboost-all-dev libfltk1.3-dev libeigen3-dev \
   libarmadillo-dev qt5-default libqt5x11extras5-dev curl git-annex lcov libxml2-utils
 
-if [ "$HOSTING_OS" == "xenial" ] || [ "$HOSTING_OS" == "trusty" ]; then
+if [ "$HOSTING_OS" = "xenial" ] || [ "$HOSTING_OS" = "trusty" ]; then
   sudo apt-get install -y -qq python3.5 python3.5-dev
   sudo update-alternatives --install /usr/local/bin/python python /usr/bin/python3.5 1 --force
 else
@@ -28,7 +28,7 @@ sudo apt-get install -qq -y gfortran-9
 
 sudo update-alternatives --install /usr/bin/gfortran gfortran /usr/bin/gfortran-9 90
 
-if ["$HOSTING_COMPILER" == "gcc"]; then
+if [ "$HOSTING_COMPILER" = "gcc" ]; then
   sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-9 90 --slave /usr/bin/g++ g++ /usr/bin/g++-9
 fi
 
